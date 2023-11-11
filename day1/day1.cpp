d9c7f0f60b9c9a7b8fcb465b7bb8def524380f5b
palnyseth <paalnyseth@gmail.com>
2023-11-11 23:19:33 +0100

    actually added (removed?) files from git lol

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -2,15 +2,29 @@
 #include <iostream>
 #include <fstream>
 int main () {
-	
-	std::ifstream input("test_input.txt");
-	if (input.is_open()) {
-		std::cerr << "failed to open file" << std::endl;
 
-		return 1;
-	}
+    std::ifstream input("test_input.txt");
+    if (!input.is_open()) {
+        std::cerr << "failed to open file" << std::endl;
 
-	int sum = 0;
+        return 1;
+    }
 
-	return 0;
+    int numberGroupOne = 0;
+    std::string line;
+
+    while (std::getline(input, line)) {
+        int num = 0;
+        if (line.empty()) {
+            num = 0;
+        } else {
+            num = std::stoi(line); 
+        }
+            numberGroupOne += num;
+    }
+
+    input.close();
+
+    std::cout << numberGroupOne << std::endl;
+    return 0;
 }
