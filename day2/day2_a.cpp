aa37f02a66fef54ce68f5d99dc971480844beb73
palnyseth <paalnyseth@gmail.com>
2023-11-12 12:05:22 +0100

    day2_a init

---
+++ day2/day2_a.cpp
@@ -0,0 +1,13 @@
+// First column - opponent is gonna play: A - rock, B - paper, C - scissors
+// Second column - what you should play?: X - rock, Y - paper, Z - scissors 
+// Winner = highest score, total score is score for each round. 
+// Score per round is score for shape, 1 - rock, 2 - paper, 3 - scissors + outcome (0 if loss, 3 if draw, 6 if win)
+//
+// Round 1: opponent -> A, you -> Y, = 8 score (paper + win)
+// Round 2: opponent -> B, you -> X, = 1 score (rock + loss)
+// Round 3: opponent -> C, you -> Z, = 6 score (scissor + draw)
+// Total score: 15
+
+#include <vector>
+int main() {
+}
