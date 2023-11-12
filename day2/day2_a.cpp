ca3b60911649a571538d76a218e46d64d9add8c5
palnyseth <paalnyseth@gmail.com>
2023-11-12 23:53:51 +0100

    day2 stuff ig?

--- day2/day2_a.cpp
+++ day2/day2_a.cpp
@@ -8,6 +8,54 @@
 // Round 3: opponent -> C, you -> Z, = 6 score (scissor + draw)
 // Total score: 15
 
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
 #include <vector>
+
+using namespace std;
+
+
+map<string, int> initPlayScoreMap() {
+    map<string, int> playScoreMap;
+    playScoreMap["rock"] = 1;
+    playScoreMap["paper"] = 2;
+    playScoreMap["scissors"] = 3;
+    return playScoreMap;
+}
+
+map<string, int> initResultScoreMap() {
+    map<string, int> resultScoreMap;
+    resultScoreMap["LOSS"] = 0;
+    resultScoreMap["DRAW"] = 3;
+    resultScoreMap["WIN"] = 6;
+    return resultScoreMap;
+}
+
+string trimAllWhitespace(const string& str) {
+    string result = str;
+    result.erase(remove_if(result.begin(), result.end(), ::isspace), result.end());
+    return result;
+}
+
 int main() {
+    vector<int> roundScores;
+    ifstream input("input.txt");
+    if(!input.is_open()) {
+        cerr << "failed to open file." << endl;
+        return 1;
+    }
+
+    string line;
+
+    map<string, int> playScores = initPlayScoreMap();
+    map<string, int> resultScores = initResultScoreMap();
+
+    while(getline(input, line)) {
+        line = trimAllWhitespace(line);
+
+    }
 }
