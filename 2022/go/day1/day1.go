0fbf940c4c26c284cb9de3eba1073885a0e19ea3
palnyseth <paalnyseth@gmail.com>
2023-11-20 00:50:33 +0100

    part 2 ok

--- 2022/go/day1/day1.go
+++ 2022/go/day1/day1.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
-    "kursiv/aoc/util"
+	"kursiv/aoc/util"
+	"sort"
 )
 
 func main() {
-    lines := util.Get_input_int("input.txt")
+    input := util.Get_input_int("input.txt")
+    ans1 := part1(input) 
+    fmt.Println("Anwer part 1: ", ans1)
+
+    
+    ans2 := part2(input) 
+    fmt.Println("Anwer part 2: ", ans2)
+}
+
+func part1(input []int) int {
     total := []int{}
     var sum int;
-    for _, value := range lines { 
+    for _, value := range input { 
         if value == 0 {
             total = append(total, sum) 
             sum = 0
@@ -17,5 +27,23 @@ func main() {
         sum += value 
     }
     ans := util.FindMax(total)
-    fmt.Println("Anwer: ", ans)
+    return ans
+}
+
+func part2(input []int) int {
+    total := []int{}
+    var sum int;
+    for _, value := range input { 
+        if value == 0 {
+            total = append(total, sum) 
+            sum = 0
+        }
+        sum += value 
+    }
+    sort.Ints(total)
+    var topSum int
+    for i := 0; i < 3; i++ {
+        topSum += total[len(total)-1-i]
+    }
+    return topSum
 }
