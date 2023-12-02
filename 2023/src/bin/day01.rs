7756101409fca448fc606035e825e31d14bd31e3
kursivxyz <paalnyseth@gmail.com>
2023-12-03 00:12:44 +0100

    rust day 1 p1

---
+++ 2023/src/bin/day01.rs
@@ -0,0 +1,16 @@
+fn main(){
+    let input = include_str!("./input/01.txt");
+    println!("Part 1: {}", part1(input)); 
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            (line.chars().find_map(|a| a.to_digit(10)).unwrap()) * 10 + 
+            (line.chars().rev().find_map(|a| a.to_digit(10)).unwrap())
+        })
+        .sum()
+}
+
